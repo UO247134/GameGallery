ee6bf42cd77a0814e6ee78dce29fdc2aa2d1a299
oscar134 <UO265078@uniovi.es>
2019-12-26 13:17:58 +0100

    intentando poner firebase

--- app/src/main/java/com/example/gamegallery/datos/Datos.kt
+++ app/src/main/java/com/example/gamegallery/datos/Datos.kt
@@ -1,6 +1,10 @@
 package com.example.gamegallery.datos
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.example.gamegallery.domain.Juego
+
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Datos (){
 
@@ -8,6 +12,19 @@ class Datos (){
 
     companion object {
         fun getJuegos(nombreConsola:String) : List<Juego>{
+            val db = FirebaseFirestore.getInstance()
+
+            db.collection("juegos")
+                    .get()
+                    .addOnSuccessListener { result ->
+                        for (document in result) {
+                            Log.d(TAG, "${document.id} => ${document.data}")
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w(TAG, "Error getting documents.", exception)
+                    }
+
             val juegos : List<Juego> = listOf(Juego("PES 2020","https://sm.ign.com/ign_es/screenshot/default/blob_hags.png","PS4"),
                     Juego("Dark Souls 3","https://i.blogs.es/984dc1/cc76dfed12ccb88d8abcc327922f3c15/450_1000.png","3rd Party"))
             if(nombreConsola.equals("All"))
@@ -16,4 +33,6 @@ class Datos (){
             return toRet
         }
     }
+
+
 }
